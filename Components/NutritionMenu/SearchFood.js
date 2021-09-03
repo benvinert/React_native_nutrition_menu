f9568b19bdd80a78953f7463afbbfc1d8105de3d
benvinert <benvinerttt@gmail.com>
2021-09-03 20:56:33 +0300

    add pie chart to values of food

--- Components/NutritionMenu/SearchFood.js
+++ Components/NutritionMenu/SearchFood.js
@@ -1,28 +1,50 @@
 import React, { useState, useMemo, useContext } from "react";
 import Autocomplete from "react-native-autocomplete-input";
-import { StyleSheet, View, StatusBar, Button } from "react-native";
+import { StyleSheet, View, StatusBar } from "react-native";
 import { metafoods } from "../../Utils/metafoods";
 import { AutocompleteDropdown } from "react-native-autocomplete-dropdown";
 import ValuesOfFood from "./ValuesOfFood";
 import { serverPath, getFood } from "../../Utils/EndPoints";
 import { createMenuContext } from "./Context/createMenuContext";
+import { Tooltip, Text } from "react-native-elements";
+import { useToast } from "react-native-styled-toast";
+import { Button } from "react-native-elements";
+import Icon from "react-native-vector-icons/FontAwesome";
 
 export const AutoComplete = ({ route }) => {
   const { menuState, menuDispatch } = useContext(createMenuContext);
   const [selectedItem, setSelectedItem] = useState(null);
   const [valuesOfFood, setValuesOfFood] = useState(null);
   const [showValues, setShowValues] = useState(false);
+  const [showToast, setShowToast] = useState(false);
   const indexOfMeal = route.params;
+  const { toast } = useToast();
+
   const addCurrentFoodToMenu = () => {
-    // add here logic to add food
-    menuDispatch({
-      execute: "add_food",
-      param: { valuesOfFood: valuesOfFood, indexOfMeal: indexOfMeal },
-    });
+    if (valuesOfFood != null) {
+      menuDispatch({
+        execute: "ADD_FOOD",
+        param: { valuesOfFood: valuesOfFood, indexOfMeal: indexOfMeal },
+      });
+      toast({
+        message: "Added Successfully",
+        iconFamily: "FontAwesome",
+        iconName: "check-circle",
+      });
+      setSelectedItem(null);
+    } else {
+      toast({
+        message: "You're need to pick a food",
+        iconFamily: "FontAwesome",
+        iconName: "exclamation-circle",
+        accentColor: "red",
+        iconColor: "red",
+      });
+    }
   };
 
   const getFoodById = async (food) => {
-    fetch(`${serverPath}${getFood.getFoodById}${food.id}`)
+    await fetch(`${serverPath}${getFood.getFoodById}${food.id}`)
       .then((response) => response.json())
       .then((jsonResponse) => {
         setValuesOfFood({ ...jsonResponse, name: food.title });
@@ -51,6 +73,16 @@ export const AutoComplete = ({ route }) => {
       />
       <Button
         title="Add to you're menu"
+        icon={
+          <Icon
+            style={{ marginLeft: 7 }}
+            name="plus-circle"
+            iconFamily="FontAwesome"
+            size={15}
+            color="white"
+          />
+        }
+        iconRight
         onPress={() => addCurrentFoodToMenu()}
       />
       <Button title="show Menu" onPress={() => console.log(menuState)} />
